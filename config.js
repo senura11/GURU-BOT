10b5b2fbaf981e003aa6c3c194d175b047adfe24
senupama <111191247+senura11@users.noreply.github.com>
2023-05-31 12:47:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94787576427', 'sandaru', true],
+  ['94787576427', 'Senupama', true], 
   [''] 
 ] //Number of owners
 
